a58d4274f466241eb388b75143a794a02affd5ed
Christopher <Christopher.woods723@gmail.com>
2025-03-17 17:21:56 -0400

    added fetchable firestore data

---
+++ DriveShare/Views/AddCarView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCarView.swift
+//  DriveShare
+//
+//  Created by Christopher Woods on 3/17/25.
+//
+
+import SwiftUI
+
+struct AddCarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCarView()
+}
